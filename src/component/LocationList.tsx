1b014c19bdc72c84f3a205da685d7c60cc87ecdc
sharad1996 <sharadwankhade23@gmail.com>
2023-06-29 23:19:13 +0530

    Implemented pagination for location list and implemented data filter with status as well

--- src/component/LocationList.tsx
+++ src/component/LocationList.tsx
@@ -1,13 +1,41 @@
+import { Pagination } from "react-bootstrap";
 import LocationCard from "./LocationCard";
+import { useState } from "react";
 
-function LocationList({ data, setSelectedCardId }: any) {
+function LocationList({ data, setSelectedCardId, pages }: any) {
+  const [active, setActive] = useState(1);
+  const items = [];
+
+  const handleClick = (e: any, i: any) => {
+    setActive(i);
+  };
+
+  for (let i = 1; i <= Math.floor(pages / 10); i++) {
+    items.push(
+      <Pagination.Item
+        key={i}
+        active={i === active}
+        onClick={(e) => handleClick(e, i)}
+      >
+        {i}
+      </Pagination.Item>
+    );
+  }
+
   return (
     <div>
-      {data.map((item: any) => (
-        <div key={item?.id}>
-          <LocationCard item={item} setSelectedCardId={setSelectedCardId} />
-        </div>
-      ))}
+      {data?.length ? (
+        <>
+          {data.map((item: any) => (
+            <div key={item?.id}>
+              <LocationCard item={item} setSelectedCardId={setSelectedCardId} />
+            </div>
+          ))}
+          <Pagination>{items}</Pagination>
+        </>
+      ) : (
+        <h3>No location found</h3>
+      )}
     </div>
   );
 }
