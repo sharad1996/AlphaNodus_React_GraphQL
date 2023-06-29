18fd76a94c94a4779de8c320b708cc276e9d5de4
sharad1996 <sharadwankhade23@gmail.com>
2023-06-29 22:04:59 +0530

    Delete

--- src/component/LocationDetails.tsx
+++ src/component/LocationDetails.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import LocationCard from "./LocationCard";
 import { GET_LOCATION_BY_ID } from "../Apollo/Queries";
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
+import { AiFillDelete } from "react-icons/ai";
+import { REMOVE_LOCATION } from "../Apollo/Mutation";
 
 function LocationDetails({ id }: { id?: string }) {
   const { loading, error, data } = useQuery(GET_LOCATION_BY_ID, {
@@ -10,12 +12,28 @@ function LocationDetails({ id }: { id?: string }) {
       tenant: "940e8edf-edd9-401d-a21a-10f866fbdb3f",
     },
   });
+  const [removeLocation, { data: deleting }] = useMutation(REMOVE_LOCATION, {
+    variables: {
+      locationRemoveId: data?.locationRead?.id,
+      tenant: "940e8edf-edd9-401d-a21a-10f866fbdb3f",
+    },
+  });
 
   if (loading) return null;
   if (error) return `Error! ${error}`;
-  console.log("=========== data 121 ===========", data);
+  console.log("=========== data 121 1===========", data);
+  const deleteLocation = () => {
+    removeLocation()
+      .then(() => {})
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <>
+      <div>
+        delete location <AiFillDelete onClick={deleteLocation} />{" "}
+      </div>
       <div>Location Container Details page</div>
       <LocationCard />
     </>
